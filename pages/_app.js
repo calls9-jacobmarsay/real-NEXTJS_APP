f2c341fec18aed5c584f09d7b8542577146a9b7a
Jacob Marsay <jacobmarsay@calls9.com>
2023-03-01 16:14:24 +0000

    wrapping component with the Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-// test
+import Layout from '../components/layout/Layout'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    );
 }
 
 export default MyApp
